b2f329c181232f98e710d02a932daec0134735af
sedemmler <sedemmler@gmail.com>
2020-03-26 13:55:26 -0400

    Adding basic kelly criterion information. Added kelly_size argument for risk management flexibility

--- WagerBrain/bankroll.py
+++ WagerBrain/bankroll.py
@@ -5,6 +5,12 @@ Bankroll management functions
 """
 
 
-def basic_kelly_criterion(prob, odds):
-    return round(((prob * odds) - 1) / (odds - 1), 2)
+def basic_kelly_criterion(prob, odds, kelly_size=1):
+    """
+    :param prob: Estimated probability of winning the wager
+    :param odds: Stated odds from bookmaker
+    :param kelly_size: Risk management. (e.g., 1 is Kelly Criterion, .5 is Half Kelly, 2+ is Levered Kelly)
+    :return: % of bankroll one should commit to wager
+    """
+    return round((((prob * odds) - 1) / (odds - 1) * kelly_size), 2)
 
